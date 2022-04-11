cd51f3f2185ccf974532a4e314aaaff3a672fa60
Ted Schelble <ted.h.schelble@gmail.com>
2022-04-11 11:52:06 -0700

    First full build multiple rounds

--- application.py
+++ application.py
@@ -16,35 +16,39 @@ num_players = st.selectbox('Input the number of players from 4 to 40', [
     x for x in range(4, 41)])
 f'You selected: {num_players} players'
 match_type = st.selectbox('Select the match type', ['', 'Singles', 'Doubles'])
+rounds = st.selectbox('Select number of rounds', [x for x in range(1, 11)])
 
-if num_players and match_type:
+if num_players and match_type and rounds:
     players = [f'Player {x}' for x in range(1, num_players + 1)]
     game_size = 4 if match_type == 'Doubles' else 2
     matches = num_players // game_size + \
         1 if num_players % game_size != 0 else num_players // game_size
+    history = {}
+    for player in players:
+        history[player] = {'teammates': [], 'opponents': []}
+    # history = {'Player 1': {'teammates': ['Player 3', 'Player 4', 'Player 5'], 'opponents': ['Player 3','Player 6', 'Player 7']}}
 
-    f'Calculating {matches} matches...'
+    f'Calculating {matches * rounds} matches...'
 
-    random.shuffle(players)
-    results = arrange_players(players, game_size)
-
-    # Add a placeholder
-    latest_iteration = st.empty()
+    # Initialize progress bar
     bar = st.progress(0)
 
-    for i in range(10):
-        # Update the progress bar with each iteration.
-        # latest_iteration.text(f'Iteration {i+1}')
-        bar.progress((i + 1) * 10)
-        time.sleep(0.1)
+    for i in range(rounds):
+        # Iterate over desired rounds
 
-    st.subheader('Random Matches')
-    st.dataframe(results)  # add a styler?
+        # Get results and updated history each time
+        results, history = arrange_players(
+            players, game_size, history, iterations=10)
+
+        # Update the progress bar with each iteration.
+        bar.progress((i + 1) / rounds)
+
+        # Output results
+        st.subheader(f'Round {i + 1}')
+        st.dataframe(results)  # add a styler?
 
 # Pseudocode
-# Add rounds as an input
-# Determine iterations based on rounds
-# Determine random weights for playing with same player or against same player
+# Determine random weights for playing with same player (2) or against same player (1)
 # Do X iterations, and find a combo with minimum weights
 # Store history of who played with who and who played against who
 # Use this history to calculate score
--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
+from re import L
 import pandas as pd
+from random import shuffle
 
 
 def chunks(lst, n):
@@ -26,7 +28,148 @@ def combine_doubles(teams):
     return df
 
 
-def arrange_players(players, game_size):
+def check_teammate(history, player1, player2):
+    return player2 in history[player1].get('teammates')
+
+
+def check_opponent(history, player1, player2):
+    return player2 in history[player1].get('opponents')
+
+
+def score_overlap(df, history):
+    '''Calculate overlap score for a df given the history'''
+    score = 0
+    teammate_weight = 2
+    opponent_weight = 1
+    if len(history) == 0:
+        return score
+    for index, row in df.iterrows():
+        players = row['Team 1'].split(', ') + row['Team 2'].split(', ')
+        if len(players) == 2:
+            if check_opponent(history, players[0], players[1]):
+                score += opponent_weight
+        elif len(players) == 3:
+            if check_teammate(history, players[0], players[1]):
+                score += teammate_weight
+            if check_opponent(history, players[0], players[2]):
+                score += opponent_weight
+            if check_opponent(history, players[1], players[2]):
+                score += opponent_weight
+        elif len(players) == 4:
+            if check_teammate(history, players[0], players[1]):
+                score += teammate_weight
+            if check_teammate(history, players[2], players[3]):
+                score += teammate_weight
+            if check_opponent(history, players[0], players[2]):
+                score += opponent_weight
+            if check_opponent(history, players[0], players[3]):
+                score += opponent_weight
+            if check_opponent(history, players[1], players[2]):
+                score += opponent_weight
+            if check_opponent(history, players[1], players[3]):
+                score += opponent_weight
+    return score
+
+
+def add_teammate(history, player1, player2):
+    history[player1]['teammates'].append(player2)
+    history[player2]['teammates'].append(player1)
+    return history
+
+
+def add_opponent(history, player1, player2):
+    history[player1]['opponents'].append(player2)
+    history[player2]['opponents'].append(player1)
+    return history
+
+
+def history_append(df, history):
+    '''Add the finalized round to our existing history'''
+    for index, row in df.iterrows():
+        players = row['Team 1'].split(', ') + row['Team 2'].split(', ')
+        if len(players) == 2:
+            history = add_opponent(history, players[0], players[1])
+        elif len(players) == 3:
+            history = add_teammate(history, players[0], players[1])
+            history = add_opponent(history, players[0], players[2])
+            history = add_opponent(history, players[1], players[2])
+        elif len(players) == 4:
+            history = add_teammate(history, players[0], players[1])
+            history = add_teammate(history, players[2], players[3])
+            history = add_opponent(history, players[0], players[2])
+            history = add_opponent(history, players[1], players[2])
+            history = add_opponent(history, players[0], players[3])
+            history = add_opponent(history, players[1], players[3])
+    return history
+
+
+def build_match(players, game_size):
+    '''Build a random match to evaluate overlap score'''
+    if len(players) % game_size == 0:
+        # Perfectly even player distribution for singles or doubles
+        teams = list(chunks(players, game_size))
+        if game_size == 2:
+            df = pd.DataFrame(teams, columns=['Team 1', 'Team 2'])
+        else:
+            df = combine_doubles(teams)
+    elif len(players) % game_size == 1:
+        # One additional player
+        if game_size == 2:
+            # Create all singles matches and one canadian doubles match
+            three = players[0:3]
+            players = players[3:]
+            teams = list(chunks(players, game_size))
+            df = pd.DataFrame(teams, columns=['Team 1', 'Team 2'])
+            df2 = pd.DataFrame(
+                [[three[0] + ', ' + three[1], three[2]]], columns=['Team 1', 'Team 2'])
+            df = pd.concat([df, df2], ignore_index=True)
+        elif game_size == 4:
+            # Create all doubles matches, 1 singles match, and one canadian doubles match
+            five = players[0:5]
+            players = players[5:]
+            teams = list(chunks(players, game_size))
+            df = combine_doubles(teams)
+            df2 = pd.DataFrame(
+                [[five[0] + ', ' + five[1], five[2]], [five[3], five[4]]], columns=['Team 1', 'Team 2'])
+            df = pd.concat([df, df2], ignore_index=True)
+    elif len(players) % game_size == 2:
+        # Create all doubles matches, 1 singles match
+        two = players[0:2]
+        players = players[2:]
+        teams = list(chunks(players, game_size))
+        df = combine_doubles(teams)
+        df2 = pd.DataFrame([[two[0], two[1]]], columns=['Team 1', 'Team 2'])
+        df = pd.concat([df, df2], ignore_index=True)
+    elif len(players) % game_size == 3:
+        # Create all doubles matches, 1 canadian doubles match
+        three = players[0: 3]
+        players = players[3:]
+        teams = list(chunks(players, game_size))
+        df = combine_doubles(teams)
+        df2 = pd.DataFrame(
+            [[three[0] + ', ' + three[1], three[2]]], columns=['Team 1', 'Team 2'])
+        df = pd.concat([df, df2], ignore_index=True)
+    return df.copy()
+
+
+def arrange_players(players, game_size, history, iterations):
+    '''Test different builds to compute min overlap'''
+    for i in range(iterations):
+        shuffle(players)
+        curr_df = build_match(players, game_size)
+        curr_score = score_overlap(curr_df, history)
+        if curr_score == 0:
+            best_df = curr_df
+            break
+        elif i == 0 or curr_score < min_score:
+            best_df = curr_df
+            min_score = curr_score
+
+    history = history_append(best_df, history)
+    return best_df, history
+
+
+def og_arrange_players(players, game_size, history):
     '''Arrange players into teams given game size'''
     if len(players) % game_size == 0:
         # Perfectly even player distribution for singles or doubles
@@ -75,4 +218,4 @@ def arrange_players(players, game_size):
     else:
         # Error handling
         df = pd.DataFrame([['Try again']], columns=['Error, Sorry!'])
-    return df
+    return df, history
